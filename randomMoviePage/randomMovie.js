1278806b1e69a1d3220b0912ae2ed984d9636e11
mageunwon <geunwon1947@gmail.com>
2022-10-03 23:14:54 +0900

    일단 포스터 180도 회전

---
+++ randomMoviePage/randomMovie.js
@@ -0,0 +1,28 @@
+const random = {
+  module:document.getElementById('module'),
+  card:document.getElementById('card'),
+  drawer:document.getElementById('drawer'),
+  front:document.getElementById('front'),
+  back:document.getElementById('back')
+}
+
+let isClick = true;
+
+random.drawer.addEventListener('click', ()=>{
+  if(isClick===true){
+    isClick = false;
+    console.log(isClick);
+
+    random.card.style.transformStyle = "preserve-3d";
+    random.card.style.transform = "rotateY(180deg)";
+    random.card.style.transition = "1s";
+  } else{
+    isClick = true;
+    console.log(isClick);
+
+    random.card.style.transformStyle = "preserve-3d";
+    random.card.style.transform = "rotateY(0)";
+    random.card.style.transition = "1s";
+    random.front.style.backfaceVisibility = "visible";
+  }
+})
